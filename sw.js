63714c48dce6262a7264515bf078521de7654ec5
ELLIEROD <megapixda20k@gmail.com>
2026-06-26 23:54:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'qa-v2'; // Mudei para v2 para o celular do pessoal atualizar!
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/manifest.json',
+  '/favicon.ico',     // Adicionado
+  '/icon-192.png',    // Adicionado
+  '/icon-512.png',    // Adicionado
+  '/js/config.js',
+  '/js/auth.js',
+  '/js/dashboard.js'
+];
+
+// Instalação do Service Worker e Cache dos arquivos essenciais
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Resposta com Cache ou Rede
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
